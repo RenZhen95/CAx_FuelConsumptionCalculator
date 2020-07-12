ef6c47f7e73b6fc1cf9f6911e6159699b7fbdba3
Jonathan Liaw <j-liaw@hotmail.com>
2020-07-12 10:23:50 +0200

    Add project files.

---
+++ CAx_FuelConsumptionCalculator/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CAx_FuelConsumptionCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LiterKilometerBox = new MetroFramework.Controls.MetroTextBox();
+            this.MilesGallonBox = new MetroFramework.Controls.MetroTextBox();
+            this.LKMtoMPG = new MetroFramework.Controls.MetroButton();
+            this.MPGtoLKM = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // LiterKilometerBox
+            // 
+            // 
+            // 
+            // 
+            this.LiterKilometerBox.CustomButton.Image = null;
+            this.LiterKilometerBox.CustomButton.Location = new System.Drawing.Point(92, 2);
+            this.LiterKilometerBox.CustomButton.Name = "";
+            this.LiterKilometerBox.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.LiterKilometerBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.LiterKilometerBox.CustomButton.TabIndex = 1;
+            this.LiterKilometerBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.LiterKilometerBox.CustomButton.UseSelectable = true;
+            this.LiterKilometerBox.CustomButton.Visible = false;
+            this.LiterKilometerBox.Lines = new string[0];
+            this.LiterKilometerBox.Location = new System.Drawing.Point(40, 120);
+            this.LiterKilometerBox.MaxLength = 32767;
+            this.LiterKilometerBox.Name = "LiterKilometerBox";
+            this.LiterKilometerBox.PasswordChar = '\0';
+            this.LiterKilometerBox.PromptText = "Enter Value";
+            this.LiterKilometerBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.LiterKilometerBox.SelectedText = "";
+            this.LiterKilometerBox.SelectionLength = 0;
+            this.LiterKilometerBox.SelectionStart = 0;
+            this.LiterKilometerBox.ShortcutsEnabled = true;
+            this.LiterKilometerBox.Size = new System.Drawing.Size(120, 30);
+            this.LiterKilometerBox.TabIndex = 0;
+            this.LiterKilometerBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.LiterKilometerBox.UseSelectable = true;
+            this.LiterKilometerBox.WaterMark = "Enter Value";
+            this.LiterKilometerBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.LiterKilometerBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // MilesGallonBox
+            // 
+            // 
+            // 
+            // 
+            this.MilesGallonBox.CustomButton.Image = null;
+            this.MilesGallonBox.CustomButton.Location = new System.Drawing.Point(92, 2);
+            this.MilesGallonBox.CustomButton.Name = "";
+            this.MilesGallonBox.CustomButton.Size = new System.Drawing.Size(25, 25);
+            this.MilesGallonBox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.MilesGallonBox.CustomButton.TabIndex = 1;
+            this.MilesGallonBox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.MilesGallonBox.CustomButton.UseSelectable = true;
+            this.MilesGallonBox.CustomButton.Visible = false;
+            this.MilesGallonBox.Lines = new string[0];
+            this.MilesGallonBox.Location = new System.Drawing.Point(240, 120);
+            this.MilesGallonBox.MaxLength = 32767;
+            this.MilesGallonBox.Name = "MilesGallonBox";
+            this.MilesGallonBox.PasswordChar = '\0';
+            this.MilesGallonBox.PromptText = "Enter Value";
+            this.MilesGallonBox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.MilesGallonBox.SelectedText = "";
+            this.MilesGallonBox.SelectionLength = 0;
+            this.MilesGallonBox.SelectionStart = 0;
+            this.MilesGallonBox.ShortcutsEnabled = true;
+            this.MilesGallonBox.Size = new System.Drawing.Size(120, 30);
+            this.MilesGallonBox.TabIndex = 1;
+            this.MilesGallonBox.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.MilesGallonBox.UseSelectable = true;
+            this.MilesGallonBox.WaterMark = "Enter Value";
+            this.MilesGallonBox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.MilesGallonBox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // LKMtoMPG
+            // 
+            this.LKMtoMPG.BackColor = System.Drawing.Color.Gray;
+            this.LKMtoMPG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LKMtoMPG.BackgroundImage")));
+            this.LKMtoMPG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.LKMtoMPG.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.LKMtoMPG.Location = new System.Drawing.Point(184, 107);
+            this.LKMtoMPG.Name = "LKMtoMPG";
+            this.LKMtoMPG.Size = new System.Drawing.Size(30, 30);
+            this.LKMtoMPG.TabIndex = 2;
+            this.LKMtoMPG.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.LKMtoMPG.UseSelectable = true;
+            this.LKMtoMPG.Click += new System.EventHandler(this.LKMtoMPG_Click);
+            // 
+            // MPGtoLKM
+            // 
+            this.MPGtoLKM.BackColor = System.Drawing.Color.Gray;
+            this.MPGtoLKM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MPGtoLKM.BackgroundImage")));
+            this.MPGtoLKM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.MPGtoLKM.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.MPGtoLKM.Location = new System.Drawing.Point(184, 145);
+            this.MPGtoLKM.Name = "MPGtoLKM";
+            this.MPGtoLKM.Size = new System.Drawing.Size(30, 30);
+            this.MPGtoLKM.TabIndex = 3;
+            this.MPGtoLKM.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.MPGtoLKM.UseSelectable = true;
+            this.MPGtoLKM.Click += new System.EventHandler(this.MPGtoLKM_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.Location = new System.Drawing.Point(37, 98);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(115, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Liter per 100km";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel2.Location = new System.Drawing.Point(237, 98);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(118, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Miles per Gallon";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.MPGtoLKM);
+            this.Controls.Add(this.LKMtoMPG);
+            this.Controls.Add(this.MilesGallonBox);
+            this.Controls.Add(this.LiterKilometerBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Fuel Consumption Converter";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox LiterKilometerBox;
+        private MetroFramework.Controls.MetroTextBox MilesGallonBox;
+        private MetroFramework.Controls.MetroButton LKMtoMPG;
+        private MetroFramework.Controls.MetroButton MPGtoLKM;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ CAx_FuelConsumptionCalculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Globalization;
+using MetroFramework.Forms;
+
+namespace CAx_FuelConsumptionCalculator
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LKMtoMPG_Click(object sender, EventArgs e)
+        {
+            double convertedValue, nominator, denominator;
+
+            // First check the value input by the user
+            (bool, double) checkResults = checkUserInput(LiterKilometerBox.Text);
+            if (!checkResults.Item1) { return; }
+            else
+            {
+                nominator   = 62.13712;     // 100km in Miles
+                denominator = 0.26417205;   // 1 Liter in Gallons
+
+                // Convert L/100km into MpG
+                convertedValue = nominator / (denominator * checkResults.Item2);
+
+                MilesGallonBox.Text = convertedValue.ToString();
+            }
+        }
+
+        private void MPGtoLKM_Click(object sender, EventArgs e)
+        {
+            double convertedValue, nominator, denominator;
+
+            // First check the value input by the user
+            (bool, double) checkResults = checkUserInput(MilesGallonBox.Text);
+            if (!checkResults.Item1) { return; }
+            else
+            {
+                nominator   = 3.78541182;       // 1 Gallon in Liter
+                denominator = 1 / (62.13712);   // Miles in 100km
+
+                // Convert MpG into L/100km
+                convertedValue = nominator / (denominator * checkResults.Item2);
+
+                LiterKilometerBox.Text = convertedValue.ToString();
+            }
+
+        }
+
+        /// <summary>
+        /// Check if the user input a double parsable string and returns that value
+        /// </summary>
+        /// <param name="_userInput"></param>
+        /// <returns></returns>
+        private (bool, double) checkUserInput(string _userInput)
+        {
+            bool goodToGo;
+
+            // First replace the , with .
+            _userInput = _userInput.Replace(',', '.');
+            
+            if (Double.TryParse(_userInput, NumberStyles.Any, CultureInfo.InvariantCulture, out double _userInputDouble))
+            {
+                goodToGo = true;
+                return (goodToGo, _userInputDouble);
+            }
+            else
+            {
+                goodToGo = false;
+                return (goodToGo, 0.0);
+            }
+        }
+    }
+}
